af90dc3e1dfd15bafaec4b5470625d637298fbc2
Raditya <radit@student.uns.ac.id>
2024-01-21 02:12:16 +0700

    add authorization token on fetcher

--- helper/fetcher.js
+++ helper/fetcher.js
@@ -1,14 +1,24 @@
 const fetcher = async ({ url, method = "GET", body = null, headers = {} }) => {
   try {
+    const token = localStorage.getItem("token");
+
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, {
-      headers,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        ...headers,
+      },
       method,
       body: body ? JSON.stringify(body) : null,
     });
     const result = await response.json();
     return result;
   } catch (error) {
-    console.log(error);
+    if (error.message === "You are not logged in") {
+      // localStorage.removeItem("token");
+      // window.location.href = "/login";
+    } else {
+      throw new Error(error.message);
+    }
   }
 };
 
